35df97c8be048d1e8460c5bd3f156dd5450922a3
Beath Here <oldbeath@gmail.com>
2026-04-29 00:31:42 +0700

    init

--- index.ts
+++ index.ts
@@ -118,6 +118,7 @@ App.post('/player/growid/login/validate', (req: Request, res: Response) => {
 
     const growId = parsedParams.get('growId') || '';
     const password = parsedParams.get('password') || '';
+    const data = parseGrowtopiaPacket(raw);
     
     // Ambil token dari form, ATAU buat baru kalau tidak ada
     const token = parsedParams.get('_token') || Buffer.from(`${growId}:${password}`).toString('base64');
@@ -128,7 +129,7 @@ App.post('/player/growid/login/validate', (req: Request, res: Response) => {
     console.log('\n🔑 [VALIDATE ROUTE] Mengecek Data Login...');
     console.log(`👤 GrowID   : ${growId === '' ? '(KOSONG / GUEST)' : growId}`);
     console.log(`🔒 Password : ${password === '' ? '(KOSONG)' : password}`);
-    console.log(`🎟️ Token    : ${token}`);
+    console.log(`🎟️ Raw    : ${data}`);
     console.log('==========================================\n');
 
     const response = {
